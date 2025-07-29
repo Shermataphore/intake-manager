12cc8af899284f882ad8c5634a4f40a402a369f9
Shermataphore <34439134+Shermataphore@users.noreply.github.com>
2025-07-28 22:41:49 -0700

    Update and rename Vendor_Products to Vendor_Products.py

--- gui/tabs/Vendor_Products.py
+++ gui/tabs/Vendor_Products.py
@@ -1,8 +1,8 @@
-# gui/tabs/active_manifest.py
+# gui/tabs/Vendor_Products.py
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
 
 class ActiveManifestTab(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout(self)
-        layout.addWidget(QLabel("Active Manifest Placeholder"))
+        layout.addWidget(QLabel("Vendor Products Placeholder"))
